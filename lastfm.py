535162d6ae3827b22d438177a770b7478918c110
James Sully <sullyj3@gmail.com>
2014-12-11 22:19:47 +1100

    initial++

---
+++ lastfm.py
@@ -0,0 +1,32 @@
+import requests
+
+apiroot = 'http://ws.audioscrobbler.com/2.0/'
+
+def get_api_key():
+    try:
+        f = open("apikey.txt")
+    except FileNotFoundError:
+        print("FAILED: No apikey.txt found in directory")
+        return None
+
+    key = f.readline().strip()
+    return key
+
+api_key = get_api_key()
+
+def getter(method, params, apikey=api_key, json=False):
+
+    assert type(method) == str
+    assert type(params) == dict
+
+    url = apiroot + '?'
+    url += "method=" + method
+
+    for param in params:
+        url += '&' + param + '=' + params[param]
+
+    url += '&api_key=' + apikey
+
+    text = requests.get(url).text
+
+    return text
